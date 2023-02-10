6b9fc729409800d54f073d4a0009effa4d3836ff
LadySkywalker <113556673+LadySkywalker@users.noreply.github.com>
2023-02-10 18:19:37 +0000

    commit 2 en nueva rama

--- EncarnaFG.php
+++ EncarnaFG.php
@@ -1,7 +1,7 @@
 <? php
 echo "Es la tercera vez que hago esto"
   echo " commit1------comit1 en nueva rama"
-  echo " Commit 2"
+  echo " Commit 2-------commit2 en nueva rama"
   echo " commit 3"
   
 ?>
