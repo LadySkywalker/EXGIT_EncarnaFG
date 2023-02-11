c86dbcfe25fc97face3894da37a3c30d820a0b23
EscoriaRebelde <124387382+EscoriaRebelde@users.noreply.github.com>
2023-02-11 16:47:01 +0000

    Deberias hacerlo asi

--- EncarnaFG.php
+++ EncarnaFG.php
@@ -1,7 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> APRENDIENDO GIT HUB</title>
+      </head>
+  <body>
 <? php
 echo "Es la tercera vez que hago esto"
   echo " commit1------comit1 en nueva rama"
   echo " Commit 2-------commit2 en nueva rama"
   echo " commit 3---------commit3 en nueva rama"
-  
+  echo
 ?>
+  </body>
+</html>
